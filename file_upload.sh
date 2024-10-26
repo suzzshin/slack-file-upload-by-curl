a862af31df35776258036ed9e0c0008af78e0f32
Shinya Suzuki <suzzshin@gmail.com>
2024-10-26 10:48:07 +0900

    add invoke function

--- file_upload.sh
+++ file_upload.sh
@@ -1,25 +1,53 @@
 #!/bin/bash
 
-function usage() {
-  echo "Usage: $0 <slack_token> <channel_id> <initial_comment> <file1> <file2> ..."
-  exit 1
+# Usage:
+# execute_file_upload <slack_token> <channel_id> <initial_comment> <file1> <file2> ...
+function execute_file_upload() {
+    local slack_token=$1
+    local channel_id=$2
+    local initial_comment=$3
+    shift 3
+    local files=$@
+
+    # トークンチェック
+    if [ -z ${slack_token} ]; then
+        echo "slack_token is required"
+        exit 1
+    fi
+
+    # チャンネルチェック
+    if [ -z ${channel_id} ]; then
+        echo "channel_id is required"
+        exit 1
+    fi
+
+    # ファイルチェック
+    for file in ${files}; do
+        if [ ! -f ${file} ]; then
+            echo "File not found: ${file}"
+            exit 1
+        fi
+    done
+
+    local filelist=()
+    local comma=""
+    for file in ${files}; do
+        local response=$(upload_file ${slack_token} ${file})
+        local upload_url=$(echo ${response} | jq -r '.upload_url')
+        local file_id=$(echo ${response} | jq -r '.file_id')
+
+        post_file ${upload_url} ${file}
+
+        filelist+=(printf '%s {"id": "%s", "title": "%s"}', ${comma} ${file_id} ${file})
+        comma=","
+    done
+
+    complete_upload ${slack_token} ${channel_id} ${initial_comment} ${filelist}
 }
 
 function upload_file() {
   local slack_token=$1
   local file_path=$2
-
-  # トークンチェック
-  if [ -z ${slack_token} ]; then
-    echo "slack_token is required"
-    exit 1
-  fi
-
-  # ファイル存在チェック
-  if [ ! -f ${file_path} ]; then
-    echo "File not found: ${file_path}"
-    exit 1
-  fi
 
   local file_name=$(basename ${file_path})
   local file_size=$(wc -c < ${file_path})
@@ -37,8 +65,6 @@ function upload_file() {
     exit 1
   fi
 
-  local upload_url=$(echo ${response} | jq -r '.upload_url')
-  local file_id=$(echo ${response} | jq -r '.file_id')
 
   return ${response}
 }
@@ -46,12 +72,6 @@ function upload_file() {
 function post_file() {
   local upload_url=$1
   local file_path=$2
-
-  # ファイル存在チェック
-  if [ ! -f ${file_path} ]; then
-    echo "File not found: ${file_path}"
-    exit 1
-  fi
 
   # sample response:
   # OK - 123456
@@ -69,8 +89,7 @@ function complete_upload() {
   local slack_token=$1
   local channel_id=$2
   local initial_comment=$3
-  local file_id=$2
-  local title=$3
+  local files=$4
 
   # ここで "error":"channel_not_found" が返却される場合は
   # アプリをチャンネルに追加する (左下のアプリをクリックして追加するチャンネル選択)
@@ -79,7 +98,7 @@ function complete_upload() {
     -H "Authorization: Bearer ${slack_token}" \
     -H "Content-Type: application/json" \
     -d '{
-      "files": [{"id":"${file_id}", "title":"${title}"}],
+      "files": ["${files}"],
       "initial_comment": "${initial_comment}",
       "channel_id": "${channel_id}"
     }' \
